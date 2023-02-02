c2e38add99be215ef3209c569cde05251bae74ed
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-01 19:23:50 -0600

    Created rest of game... maybe more commits

--- rps.js
+++ rps.js
@@ -1,35 +1,105 @@
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
+let compPick;
 
 function getComputerChoice() {
     let rng = Math.floor(Math.random()*3)
     if (rng < 1) {
-        return rock;
+        compPick = rock;
+        return compPick;
     }
     else if (rng <= 1) {
-        return paper;
+        compPick = paper;
+        return compPick;
     }
     else {
-        return scissors;
+        compPick = scissors;
+        return compPick;
     }
 }
 
+getComputerChoice();
 let decision = (prompt("Rock, Paper, or Scissors").toLowerCase());
 
 function playerChoice (decision) {
     if (decision === rock) {
-        return rock;
+        decision = rock;
+        return decision;
     }
     else if (decision === paper) {
-        return paper;
+        decision = paper;
+        return decision;
     }
     else if (decision === scissors) {
-        return scissors;
+        decision = scissors;
+        return decision;
     }
     else {
         return "Error, please pick 'rock' 'paper' or 'scissors'";
     }
+}
+
+function playGame (playerChoice) {
+    if (decision === rock){
+        checkRock();
+    }
+        
+    else if (decision === paper) {
+        checkPaper();
+    }
+        
+    else if (decision === scissors) {
+        checkScissors();
+    }  
+    else {
+        console.log("error: could not determine players choice");
+    }
+
+}
+
+
+console.log("You picked: " + decision);
+console.log("The computer picked: " +  compPick + " so...");
+playGame(decision);
+
+function checkRock() {
+    if (compPick === rock) {
+            console.log("Shame, it's a tie.");
+        }
+    else if (compPick === scissors) {
+            console.log("You win!");
+    }
+    else {
+            console.log("Sorry... you lose.")
+    }
+    
+}
+
+function checkPaper(playerChoice) {
+    if (compPick === paper) {
+            console.log("Shame, it's a tie.");
+        }
+    else if (compPick === rock) {
+            console.log("You win!");
+    }
+    else {
+            console.log("Sorry... you lose.")
+    }
+    
+}
+
+function checkScissors(playerChoice) {
+    if (compPick === scissors) {
+            console.log("Shame, it's a tie.");
+        }
+    else if (compPick  === paper) {
+            console.log("You win!");
+    }
+    else {
+            console.log("Sorry... you lose.")
+    }
+    
 }
 
 //console.log(getComputerChoice());

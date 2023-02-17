e4f2024a26c9bbb3d92ae900aad8ee52001ac7d5
ColeMcE <27023447+ColeMcE@users.noreply.github.com>
2023-02-16 18:23:44 -0600

    Achieve full function w/o DOM appends

--- rps.js
+++ rps.js
@@ -7,21 +7,59 @@ let decision;
 let compPick;
 let playerScore = 0;
 let compScore = 0;
+let clickCount = 0;
 
-//Adding event listener to each function, 
+//Reset button
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", ()=> {
+    playerScore = 0;
+    compScore = 0;
+    clickCount = 0;
+    document.getElementById("rockButton").disabled = false;
+    document.getElementById("paperButton").disabled = false;
+    document.getElementById("scissorsButton").disabled = false;
+});
+
+//Adding event listener to each button, 
 playerButtons.forEach(playerButton => {
     playerButton.addEventListener("click" , processClick);
 });
 
 //Executes one round of the game on a button click
 function processClick(){
-    getComputerChoice();
-    decision = this.value;
-    playRound(decision);
-    console.log("You picked: " + decision);
-    console.log("The computer picked: " +  compPick + " so...");
-    console.log("Your score: " + playerScore + " points, computer score: " + compScore + " points.")
-    
+    //Plays a round for the first five clicks
+    if (clickCount < 5) {
+        getComputerChoice();
+        decision = this.value;
+        playRound(decision);
+        console.log("You picked: " + decision);
+        console.log("The computer picked: " +  compPick + " so...");
+        console.log("Your score: " + playerScore + " points, computer score: " + compScore + " points.")
+        clickCount++;
+        //After five clicks tallys total and disables the buttons
+        if (clickCount == 5){
+            document.getElementById("rockButton").disabled = true;
+            document.getElementById("paperButton").disabled = true;
+            document.getElementById("scissorsButton").disabled = true;
+            tallyTotal();
+        } 
+    } 
+}
+
+//Output of results
+function tallyTotal() {
+    if (playerScore > compScore) {
+        console.log("Congrats! You won with a score of: " + playerScore + " points.")
+     }
+     else if (compScore > playerScore) {
+        console.log("Sorry, the computer won with a score of: " + compScore + " points.")
+     }
+     else if (compScore == playerScore) {
+        console.log("Welp, it was a tie overall with " + playerScore + " points.")
+     }
+     else {
+        console.log("error: failed to tally points total.")
+     }
 }
 
 //Generates a computer decision
@@ -109,6 +147,7 @@ function checkScissors() { //Supplemental function for playGame
     }
     
 }
+
 
 //console.log(getComputerChoice());
 //console.log(playerChoice(decision));
